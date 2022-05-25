5cb0b3ccebd9e20343f77b8e9f2f02382982af7d
Anar-Qandayev <tu7bxfga8@code.edu.az>
2022-05-25 04:49:24 +0400

    commit

---
+++ Nest2/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Nest2.DAL;
+using Nest2.ViewModel;
+using Nest2.ViewModel.HomeVM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nest2.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            HomeVm homeVM = new HomeVm()
+            {
+
+                Sliders = await _context.Sliders.ToListAsync(),
+                Categories = await _context.Categories.ToListAsync(),
+                Products = await _context.Products.Include(p=>p.ProductImages).Include(p => p.Category).Take(10).ToListAsync()
+            };
+
+            return View(homeVM);
+        }
+    }
+}
---
+++ Nest2/ViewModel/HomeVm.cs
@@ -0,0 +1,16 @@
+﻿using Nest2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Nest2.ViewModel.HomeVM
+{
+    public class HomeVm
+    {
+        public List<Category> Categories { get; set; }
+        public List<Slider> Sliders { get; set; }
+        public List<Product> Products { get; set; }
+        public List<ProductImg> ProductImages { get; set; }
+    }
+}
